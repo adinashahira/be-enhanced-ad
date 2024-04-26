f06ce663c7bf2efc68a342f44337c1dc23eafd69
adinashahira <aadinashahira@gmail.com>
2024-04-26 16:21:11 +0800

    feat(balancing): created balancing logic

--- booking-balancing.js
+++ booking-balancing.js
@@ -41,5 +41,54 @@ export class BookingBalancing {
 		});
 
 		console.log('current budget per streams:', streams);
+		return streams;
+	};
+
+	// Rebalance budget for all streams
+	rebalanceBudget = (streams) => {
+		// let totalRemainingBudget = reduce((acc, stream) => acc + parseFloat(stream.streamBudgetBalance), 0);
+		let totalRemainingBudget = 0;
+
+		streams.forEach((stream) => {
+			totalRemainingBudget += stream.streamBudgetBalance;
+			console.log(`check parsed for ${stream.streamName}:`, parseFloat(stream.streamUsageBalance));
+		});
+
+		let rebalancedBudgetPerStream = totalRemainingBudget / streams.length;
+		console.log('total remaining budget:', totalRemainingBudget);
+		console.log('rebalanced amount:', rebalancedBudgetPerStream);
+
+		// Case 1: Each Stream Usage Balance (%) is 0% or less
+		if (streams.every((stream) => parseFloat(stream.streamUsageBalance) <= 0)) {
+			console.log('test lalu below 0%');
+			console.log('this is the end, lalu sini');
+			return streams;
+		}
+
+		// Case 2: Each Stream Usage Balance (%) is less than or more than 5%
+		if (streams.every((stream) => parseFloat(stream.streamUsageBalance) < 5) || streams.every((stream) => parseFloat(stream.streamUsageBalance) >= 5)) {
+			console.log('Each Stream Usage Balance (%) is less than 5% or more than 5%');
+			return streams;
+		}
+
+		// Case 3: Any one or more but not all Usage Balance (%) is/are less than 5%
+		const streamsBelow5Pct = streams.filter((stream) => parseFloat(stream.streamUsageBalance) < 5);
+		console.log('test lalu any below 5%', streamsBelow5Pct.length);
+
+		if (streamsBelow5Pct.length > 0) {
+			streamsBelow5Pct.forEach((stream) => {
+				console.log(`stream ${stream.streamName}'s balance is ${stream.streamUsageBalance}`);
+			});
+
+			// Rebalancing process
+			streams.forEach((stream) => {
+				stream.streamBudget = rebalancedBudgetPerStream;
+				stream.isAdjusted = 'true';
+			});
+
+			return streams;
+		}
+
+		console.log('New streams balance:', streams);
 	};
 }
--- index.js
+++ index.js
@@ -3,12 +3,20 @@ import { BookingBalancing } from './booking-balancing.js';
 const booking = new BookingBalancing();
 
 // hardcoded values
-let budget = 15000;
+let budget = 1500;
+let action1, action2, action3;
 
 console.log('budget:', budget);
 console.log('-------------------------INITIAL------------------------------');
 const streams = booking.initializeBudgetAllocation(budget, ['TV', 'Radio', 'Podcast']);
 console.log('--------------------FIRST CALL---------------------');
-booking.randomizeConsumption(streams);
+action1 = booking.randomizeConsumption(streams);
+// console.log(action1);
 console.log('--------------------SECOND CALL---------------------');
-booking.randomizeConsumption(streams);
+action2 = booking.randomizeConsumption(streams);
+// console.log(action2);
+console.log('--------------------REBALANCE---------------------');
+action3 = booking.rebalanceBudget(action2);
+console.log('--------------------CALL AFTER REBALANCE---------------------');
+console.log('after rebalancing:', action3);
+booking.randomizeConsumption(action3);
