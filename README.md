17d375b9dd906bd80d1d8a0e204ef2ae4de73eee
adinashahira <aadinashahira@gmail.com>
2024-05-30 16:56:43 +0800

    feat: convert functions into APIs

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Enhanced Ad Booking Experience
 
 The logic for ad stream budget rebalancing is written in `booking-balancing.js`
-To test the logic, invoke the function from index file
+
+To test the logic without running it as an API, uncomment the code under **TEST BLOCK**
 
 **Example**
+
 ```
 let budget = 1500;
 let action1, action2, action3;
@@ -22,5 +24,84 @@ booking.randomizeConsumption(action3);
 ```
 
 You can change the budget or the ad streams array.
+
+The logic can also be tested as APIs using postman, refer below for sample requests
+
+API 1: Initialize Budget
+
+> POST http://localhost:8080/initialize-budget
+
+Sample Body
+
+```
+{
+    "budget": 1500,
+    "streams": ["TV", "Radio", "Podcast"]
+}
+```
+
+API 2: Consume Budget
+
+> POST http://localhost:8080/consume-budget
+
+Sample Body
+
+```
+[
+  {
+    "streamName":  "TV",
+    "previousStreamBudget": 500,
+    "streamConsumption": 0,
+    "currentStreamBudget": 500,
+    "streamUsageBalance": "100%"
+  },
+   {
+    "streamName":  "TV",
+    "previousStreamBudget": 500,
+    "streamConsumption": 0,
+    "currentStreamBudget": 500,
+    "streamUsageBalance": "100%"
+  },
+    {
+    "streamName":  "TV",
+    "previousStreamBudget": 500,
+    "streamConsumption": 0,
+    "currentStreamBudget": 500,
+    "streamUsageBalance": "100%"
+  }
+]
+```
+
+API 3: Rebalance Budget
+
+> POST http://localhost:8080/consume-budget
+
+Sample Body
+
+```
+[
+    {
+        "streamName": "TV",
+        "previousStreamBudget": 500,
+        "streamConsumption": 25,
+        "currentStreamBudget": 475,
+        "streamUsageBalance": "95.00%"
+    },
+    {
+        "streamName": "TV",
+        "previousStreamBudget": 500,
+        "streamConsumption": 491,
+        "currentStreamBudget": 9,
+        "streamUsageBalance": "1.80%"
+    },
+    {
+        "streamName": "TV",
+        "previousStreamBudget": 500,
+        "streamConsumption": 6,
+        "currentStreamBudget": 494,
+        "streamUsageBalance": "98.80%"
+    }
+]
+```
 
 To run unit test for this project, use `npm test`
--- booking-balancing.js
+++ booking-balancing.js
@@ -4,92 +4,139 @@ export class BookingBalancing {
 	}
 
 	// Initialize equal budgets for all streams
-	initializeBudgetAllocation = (budget, streams) => {
-		let numOfStreams = streams.length;
-		let initialBudgetPerStream = budget / numOfStreams;
+	initializeBudgetAllocation = (req, res) => {
+		let budget = req.body.budget;
+		let streams = req.body.streams;
+		console.log('stream:', streams);
 
-		streams.forEach((stream) => {
-			this.streamsAllocation.push({
-				streamName: stream,
-				previousStreamBudget: initialBudgetPerStream,
-				streamConsumption: 0,
-				currentStreamBudget: initialBudgetPerStream,
-				streamUsageBalance: '100%',
+		try {
+			this.streamsAllocation = [];
+			let numOfStreams = streams.length;
+			let initialBudgetPerStream = budget / numOfStreams;
+
+			streams.forEach((stream) => {
+				this.streamsAllocation.push({
+					streamName: stream,
+					previousStreamBudget: initialBudgetPerStream,
+					streamConsumption: 0,
+					currentStreamBudget: initialBudgetPerStream,
+					streamUsageBalance: '100%',
+				});
 			});
-		});
 
-		console.log('Initial budget per stream:', this.streamsAllocation);
-		return this.streamsAllocation;
+			console.log('Initial budget per stream:', this.streamsAllocation);
+
+			res.status(200).send({
+				message: 'Budget Allocated',
+				budget: this.streamsAllocation,
+			});
+
+			return this.streamsAllocation;
+		} catch (err) {
+			console.error(err);
+			res.status(400).send('Request Invalid. Processing Failure');
+		}
 	};
 
 	// Randomize consumption of budget for each streams
-	randomizeConsumption = (streams) => {
-		streams.forEach((stream) => {
-			let budget = stream.currentStreamBudget;
-			// console.log(`budget in randomize for ${stream.streamName}:`, budget);
+	consumeBudget = (req, res) => {
+		try {
+			let streams = req.body;
 
-			let consumption = Math.floor(Math.random() * (budget + 1));
-			// console.log(`consumption for ${stream.streamName}: ${consumption}`);
+			streams.forEach((stream) => {
+				let budget = stream.currentStreamBudget;
+				// console.log(`budget in randomize for ${stream.streamName}:`, budget);
 
-			let newBalance = budget - consumption;
-			// console.log(`newBalance for ${stream.streamName}:`, newBalance);
+				let consumption = Math.floor(Math.random() * (budget + 1));
+				// console.log(`consumption for ${stream.streamName}: ${consumption}`);
 
-			stream.previousStreamBudget = budget;
-			stream.streamConsumption = consumption;
-			stream.currentStreamBudget = newBalance;
-			stream.streamUsageBalance = `${((stream.currentStreamBudget / budget) * 100).toFixed(2)}%`;
-		});
+				let newBalance = budget - consumption;
+				// console.log(`newBalance for ${stream.streamName}:`, newBalance);
 
-		console.log('Budget per streams after consumption:', streams);
+				stream.previousStreamBudget = budget;
+				stream.streamConsumption = consumption;
+				stream.currentStreamBudget = newBalance;
+				stream.streamUsageBalance = `${((stream.currentStreamBudget / budget) * 100).toFixed(2)}%`;
+			});
 
-		return streams;
+			console.log('Budget per streams after consumption:', streams);
+
+			res.status(200).send({
+				message: 'Budgets consumed',
+				budget: streams,
+			});
+			return streams;
+		} catch (err) {
+			console.error(err);
+			res.status(400).send('Request Invalid. Processing Failure');
+		}
 	};
 
 	// Rebalance budget for all streams
-	rebalanceBudget = (streams) => {
-		console.log('Streams budget before rebalancing:', streams);
-		let totalRemainingBudget = 0;
+	rebalanceBudget = (req, res) => {
+		try {
+			let streams = req.body;
 
-		streams.forEach((stream) => {
-			totalRemainingBudget += stream.currentStreamBudget;
-		});
+			console.log('Streams budget before rebalancing:', streams);
+			let totalRemainingBudget = 0;
 
-		let rebalancedBudgetPerStream = totalRemainingBudget / streams.length;
-		console.log('Total Remaining Budget:', totalRemainingBudget);
-
-		// Case 1: Each Stream Usage Balance (%) is 0% or less
-		if (streams.every((stream) => parseFloat(stream.currentStreamBudget) <= 0)) {
-			console.log('Budget has been fully utilised for all ad streams');
-			return streams;
-		}
-
-		// Case 2: Each Stream Usage Balance (%) is less than or more than 5%
-		if (streams.every((stream) => parseFloat(stream.currentStreamBudget) < 5) || streams.every((stream) => parseFloat(stream.currentStreamBudget) >= 5)) {
-			console.log('Budget is still being utilised on all ad streams');
-			return streams;
-		}
-
-		// Case 3: Any one or more but not all Usage Balance (%) is/are less than 5%
-		const streamsBelow5Pct = streams.filter((stream) => parseFloat(stream.currentStreamBudget) < 5);
-		console.log("An ad stream's budget has gone below 5%, rebalancing will commence");
-		console.log('New Balanced Budget Amount:', rebalancedBudgetPerStream);
-
-		if (streamsBelow5Pct.length > 0) {
-			streamsBelow5Pct.forEach((stream) => {
-				console.log(`${stream.streamName}'s Stream Balance Is ${stream.currentStreamBudget}`);
-			});
-
-			// Rebalancing process
 			streams.forEach((stream) => {
-				stream.previousStreamBudget = rebalancedBudgetPerStream;
-				stream.currentStreamBudget = rebalancedBudgetPerStream;
-				stream.isAdjusted = 'true';
+				totalRemainingBudget += stream.currentStreamBudget;
 			});
 
-			return streams;
-		}
+			let rebalancedBudgetPerStream = totalRemainingBudget / streams.length;
+			console.log('Total Remaining Budget:', totalRemainingBudget);
 
-		console.log('New streams balance:', streams);
-		return streams;
+			// Case 1: Each Stream Usage Balance (%) is 0% or less
+			if (streams.every((stream) => parseFloat(stream.streamUsageBalance) <= 0)) {
+				console.log('Budget has been fully utilised for all ad streams');
+				res.status(200).send({
+					message: 'Budget has been fully utilised for all ad streams',
+					budget: streams,
+				});
+				return streams;
+			}
+
+			// Case 2: Each Stream Usage Balance (%) is less than or more than 5%
+			if (streams.every((stream) => parseFloat(stream.streamUsageBalance) < 5) || streams.every((stream) => parseFloat(stream.streamUsageBalance) >= 5)) {
+				console.log('Budget is still being utilised on all ad streams');
+				res.status(200).send({
+					message: 'Budget is still utilised for all ad streams',
+					budget: streams,
+				});
+				return streams;
+			}
+
+			// Case 3: Any one or more but not all Usage Balance (%) is/are less than 5%
+			const streamsBelow5Pct = streams.filter((stream) => parseFloat(stream.streamUsageBalance) < 5);
+			console.log("An ad stream's budget has gone below 5%, rebalancing will commence");
+			console.log('New Balanced Budget Amount:', rebalancedBudgetPerStream);
+
+			if (streamsBelow5Pct.length > 0) {
+				streamsBelow5Pct.forEach((stream) => {
+					console.log(`${stream.streamName}'s Stream Balance Is ${stream.currentStreamBudget}`);
+				});
+
+				// Rebalancing process
+				streams.forEach((stream) => {
+					stream.previousStreamBudget = rebalancedBudgetPerStream;
+					stream.currentStreamBudget = rebalancedBudgetPerStream;
+					stream.isAdjusted = 'true';
+				});
+				res.status(200).send({
+					message: "An ad stream's budget has gone below 5%, streams budget is rebalanced.",
+					budget: streams,
+				});
+				return streams;
+			}
+
+			console.log('New streams balance:', streams);
+
+			// res.status(200).send(streams);
+			return streams;
+		} catch (err) {
+			console.error(err);
+			res.status(400).send('Request Invalid. Processing Failure');
+		}
 	};
 }
